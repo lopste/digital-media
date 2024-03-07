bd81801f9ef4eb5d158024cda847ae1a2fa0553d
Vercte <lopstedener@gmail.com>
2024-03-07 14:11:19 -0800

    add source link

--- src/app/pages/About.tsx
+++ src/app/pages/About.tsx
@@ -4,6 +4,8 @@ export default function About() {
     return <main>
         Hello! I'm Levi Carson, some teenager who likes to program, play rhythm games, 
         and play the piano. I can't think of much else to say about me right now, so
-        that's all you get
+        that's all you get.
+
+        You can see the source code for this website here: <a href="https://github.com/lopste/digital-media">https://github.com/lopste/digital-media</a>
     </main>
 }
